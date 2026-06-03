07bbe4230608c5646506a0275597d8ac5df868fe
Oliver Stenkilde <hello@stenkil.de>
2026-06-03 13:16:50 +0200

    test: added dev embed for testing purposes

--- embed_dev.go
+++ embed_dev.go
@@ -1,11 +1,10 @@
-//go:build !dev
+//go:build dev
 
 package main
 
 import "embed"
 
-//go:embed all:web/dist
-var embeddedDist embed.FS
-
 //go:embed sql/migrations
 var migrations embed.FS
+
+var embeddedDist embed.FS
